91b8cbbfabb00e3d449e5e0d2772c80a16f4f7d5
Kisuyo <ig.intr3st@gmail.com>
2023-11-08 11:14:38 +0100

    .

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -1,7 +1,7 @@
 export default function UploadModal() {
   return (
     <>
-      <div class="w-1/3 h-fit flex flex-col gap-4 bg-white border-gray-200 border rounded-lg p-4 px-5">
+      <div class="w-1/3 h-fit z-30 flex flex-col gap-4 bg-white border-gray-200 border rounded-lg p-4 px-5">
         <div class="flex flex-col gap-1">
           <div class="text-[18px]">Upload files</div>
           <div class="text-[#909090]">
